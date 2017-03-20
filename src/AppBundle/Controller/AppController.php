66c721b79d1680e76ece749361d9b15aa99c2aef
Arthur <nienkoa@gmail.com>
2017-03-20 21:40:08 +0200

    GeniusesOfSymfony/WebSocketBundle bundle + bootstrap template

---
+++ src/AppBundle/Controller/AppController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class AppController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('AppBundle:AppController:index.html.twig', array(
+            // ...
+        ));
+    }
+
+}
